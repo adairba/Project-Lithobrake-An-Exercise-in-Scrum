b66dc79f35963e036d4e52ea4d1a5299d7513db1
Quack71 <cox.eli11@gmail.com>
2026-04-13 13:49:55 -0400

    Feat: Add projectiles when spacebar is pressed

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
+import { Shoot, UpdateProjectile, DrawProjectile } from "./modules/projectile.js"
+
 var canvas;
 var ctx;
 var WIDTH;
 var HEIGHT;
 const PLAYER_SPEED = 10;   // Speed at which the player moves.
-playerX = 175;             // Center/Home position for player. Outside of player scope to allow for persistance after changes.
-rightDown = false;
-leftDown = false;
+let playerX = 175;             // Center/Home position for player. Outside of player scope to allow for persistance after changes.
+let rightDown = false;
+let leftDown = false;
 
 // If either key press is detected, corresponding bool is set to true.
 function onKeyDown(evt)
 {
+    //spacebar shoots
+    if (evt.keyCode == 32)
+    {
+        Shoot(playerX);
+    }
     if (evt.keyCode == 37)
         leftDown = true;
     else if (evt.keyCode == 39)
@@ -33,7 +40,7 @@ function Init()
     ctx = document.getElementById("canvas").getContext("2d");
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
-    return setInterval(Player, 10);
+    //return setInterval(Player, 10);
 }
 
 //draw a square for the player
@@ -41,7 +48,7 @@ function Init()
 function Player()
 {
     // Clears the canvas of the rectangle. Without this, it smears across screen.
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    //ctx.clearRect(0, 0, WIDTH, HEIGHT);
     // Once input is detected, the X position is moved based on left/right key press.
     // Value on the right determines the speed.
     if (rightDown) 
@@ -55,7 +62,19 @@ function Player()
     ctx.fill();
 }
 
+//main game loop, everything that needs to run in an interval needs to go here
+function GameLoop()
+{
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    Player();
+    UpdateProjectile();
+    DrawProjectile(ctx);
+}
+
 Init();
+setInterval(GameLoop, 10);
+
 // Event listeners that wait for any keypress. Once a key is pressed or released, corresponding function from above is called.
 window.onkeydown = onKeyDown;
 window.onkeyup = onKeyUp;
